fb776cd28e97ec4e22cf74e89129c0d118d21405
pradeep <pradeep.kumar44@gmail.com>
2019-07-25 16:44:53 +0530

    committed the search functionality

--- day4/demo-app/js/users.js
+++ day4/demo-app/js/users.js
@@ -1,11 +1,25 @@
 let promise = fetch('https://jsonplaceholder.typicode.com/users')
 const rootElem = document.querySelector('#users');
+let data = [];
 promise
     .then(response => response.json())
-    .then(users => users.map((user)=>`<li>${user.name} X</li> `))
+    .then(users => {
+        data =  users;
+        return users.map((user)=>`<li>${user.name} X</li> `)})
     .then(data => data.join(''))
-    .then(data => rootElem.innerHTML = `<ul>${data}</ul>`);
+    .then(data =>  {
+        console.log('data is ')
+        console.log(data);
+        users = data;
+        rootElem.innerHTML = `<ul>${data}</ul>`
+    });
 
-document.querySelector('#name').addEventListener('change', function(event){
-    console.log(`coming inside the click event handler `, event.target.value)
+document.querySelector('#name').addEventListener('keyup', function(event){
+    console.log(`coming inside the click event handler `, event.target.value);
+    console.log(data)
+    let filteredUsers = data
+        .filter(user => { return user.name.startsWith(event.target.value)})
+        .map((user)=>`<li>${user.name} X</li> `)
+    rootElem.innerHTML = `<ul>${filteredUsers}</ul>`
+    
 })
